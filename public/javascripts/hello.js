ae462d2769e4cd4e96b6afdb0736f749b9f79261
= <=>
2015-06-12 22:19:32 -0300

    p5js library added, basic p5 app running

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,14 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
+(function(){
+function sketch(p){
+    p.setup = function(){
+      p.createCanvas(900,450);
+    }
+    p.draw = function(){
+      p.background(3);
+      p.rectMode(p.CENTER);
+      p.rect(p.mouseX,p.mouseY,30,30);
+    }
 }
+
+new p5(sketch,'board');
+})();
