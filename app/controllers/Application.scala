18d34efd41dc70583290f6507a2966f6d80320cb
= <=>
2015-06-12 23:42:07 -0300

    websocket already connecting with actors

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,40 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import akka.actor._
+import play.api.Play.current
+
+import scala.concurrent.Future
 
 class Application extends Controller {
+  val USER = "user";
+  var counter = 0;
 
-  def index = Action {
-    Ok(views.html.index("Hello Pong"))
+  def index = Action { implicit request =>
+    val uid: String = request.session.get(USER).getOrElse {
+      counter += 1
+      counter.toString
+    }
+    Ok(views.html.index("Hello Pong")).withSession(request.session + (USER -> counter.toString))
+  }
+
+  def socket = WebSocket.tryAcceptWithActor[String, String] { request =>
+    Future.successful(request.session.get("user") match {
+      case None => Left(Forbidden)
+      case Some(_) => Right(MyWebSocketActor.props)
+    })
+  }
+
+  object MyWebSocketActor {
+    def props(out: ActorRef) = Props(new MyWebSocketActor(out))
+  }
+
+  class MyWebSocketActor(out: ActorRef) extends Actor with ActorLogging{
+    def receive = {
+      case msg: String =>
+        log.info(msg)
+        out ! ("I received your message: " + msg)
+    }
   }
 
 }
--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,14 +1,20 @@
 (function(){
+var ws;
+
 function sketch(p){
     p.setup = function(){
       p.createCanvas(900,450);
-    }
+    };
     p.draw = function(){
       p.background(3);
       p.rectMode(p.CENTER);
-      p.rect(p.mouseX,p.mouseY,30,30);
-    }
+      p.rect(p.mouseX,p.mouseY,20,20);
+    };
 }
 
+ws = new WebSocket('ws://localhost:9000/ws');
+ws.onmessage = function(e){console.log(e.data);};
 new p5(sketch,'board');
+
+
 })();
