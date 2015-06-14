9a6eab4278d5eeed35bcd95f37faa68681ab1b25
= <=>
2015-06-14 13:42:58 -0300

    javascript p5js to joystick without functionality, no websockets yet

--- public/javascripts/joystick.js
+++ public/javascripts/joystick.js
@@ -1,4 +1,33 @@
 (function(){
 
-	console.log("Hello Joystick");
+var w = window.innerWidth,
+h = window.innerHeight,
+posY = h/2;
+
+function sketch(p){
+p.setup = function() {
+	p.createCanvas(w, h);
+};
+
+p.draw = function(){
+	p.clear();
+	p.rectMode(p.CENTER);
+	p.fill(0);
+	p.rect(w/2,h/2,(w/3)-200,h-10);
+	p.ellipseMode(p.CENTER);
+	p.fill(255,0,0);
+	p.noStroke();
+	p.ellipse(w/2,posY, w/3,w/3);
+};
+
+p.touchMoved = function(){
+	posY = p.touchY;
+};
+
+p.touchEnded = function(){
+	posY = h/2;
+};
+}
+new p5(sketch);
 })();
+
