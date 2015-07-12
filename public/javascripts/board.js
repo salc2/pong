2f2371c630580f748320155af9d2860a8bd1298f
javier salcedo <javiersalcedo75@gmail.com>
2015-07-12 20:22:22 -0300

    basic movements paddles, ball

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -14,6 +14,31 @@ function dispatch(obj){
 	document.dispatchEvent(new CustomEvent('positions',{detail: obj}));
 }
 
+function collision(o){
+var b = o.ball,
+lp = o.lpad,
+rp = o.rpad;
+b.spdy = b.y >= H-10 || 
+b.y <= 10 ? b.spdy * -1 : b.spdy;
+
+if(b.x+10 >= rp.x-10 && (rp.y+40 >= b.y-10 && rp.y-40 <= b.y+10 ) ||
+	(b.x-10 <= lp.x+10 && (lp.y+40 >= b.y-10 && lp.y-40 <= b.y+10 ))){
+   b.spdx = b.spdx * -1;
+ }
+	return o;
+}
+
+function move(o){
+	var b = o.ball;
+	b.x += b.spdx;
+	b.y += b.spdy;
+	if(b.x > W-40 || b.x < 40){
+		b.x = W/2;
+		b.y = H/2;
+	}
+}
+
+
  function sketch(p){
  var ball,
  lPaddle,
@@ -21,7 +46,7 @@ function dispatch(obj){
  
  var positions,
  paddles,
- balls;
+ moves;
 
  positions = Rx.Observable.fromEvent(document,'positions').
 	map(function(o){return o.detail;});
@@ -40,17 +65,19 @@ function dispatch(obj){
 	subscribe(function(n){
 		lPaddle.y = n.posy;
 	});
- balls = positions.filter(function(b){ return b.type === 'ball';});
-
+ moves = positions.filter(function(b){ return b.type === 'moves';});
+ moves.map(collision).subscribe(move);
  p.setup = function(){
 	 p.createCanvas(W,H);
-	 ball = {x: W/2, y: H/2, spdy: 0, spdx: 1};
+	 p.frameRate(60);
+	 ball = {x: W/2, y: H/2, spdy: 1, spdx: 4};
 	 lPaddle = {x: 40, y: H/2};
 	 rPaddle = {x: W-40, y: H/2};
 	 p.rectMode(p.CENTER);
  };
  p.draw = function(){
 	 p.background(3);
+	 dispatch({type: 'moves', ball: ball, lpad: lPaddle, rpad: rPaddle});
 	 p.rect(ball.x,ball.y,20,20);
 	 p.rect(rPaddle.x,rPaddle.y,20,80);
 	 p.rect(lPaddle.x,lPaddle.y,20,80);
