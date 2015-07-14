77043ae8e171e110500dd1424f2294685c122879
javier salcedo <javiersalcedo75@gmail.com>
2015-07-13 23:45:48 -0300

    minimizing side effect, two stream as argument

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,37 +1,39 @@
 (function(){
- var host, 
- tcp,
- path;
-
  var W = 900,
- H = 450;
-
- var ws,
+ H = 450,
+ host,
+ tcp,
+ patch,
+ ws,
  messages,
- observer;
+ observer,
+ positions,
+ paddles,
+ moves;
 
- function dispatch(obj){
+ function emitter(obj){
  document.dispatchEvent(new CustomEvent('positions',{detail: obj}));
  }
 
- 
-function collisionX(o){
-  var b = o.ball,
- lp = o.lpad,
- rp = o.rpad,
- changed = b.x+10 >= rp.x-10 && (rp.y+40 >= b.y-10 && rp.y-40 <= b.y+10 ) ||
-		 (b.x-10 <= lp.x+10 && (lp.y+40 >= b.y-10 && lp.y-40 <= b.y+10 ));
-	 return changed;
-}
 
-
-function collisionY(o){
-
+ function collisionX(o){
  var b = o.ball,
  lp = o.lpad,
- rp = o.rpad, 
- changed = (b.y >= H-10 || b.y <= 10);
+ rp = o.rpad,
+ changed = b.x+10 >= rp.x-10 && 
+	 (rp.y+40 >= b.y-10 && rp.y-40 <= b.y+10 ) ||
+	 (b.x-10 <= lp.x+10 && (lp.y+40 >= b.y-10 && 
+				lp.y-40 <= b.y+10 ));
  return changed;
+ }
+
+
+ function collisionY(o){
+	 var b = o.ball,
+	     lp = o.lpad,
+	     rp = o.rpad, 
+	     changed = (b.y >= H-10 || b.y <= 10);
+	 return changed;
  }
 
  function move(o){
@@ -44,58 +46,58 @@ function collisionY(o){
 		 b.x = W/2;
 		 b.y = H/2;
 	 }
-	return o;
+	 return o;
  }
 
 
- function sketch(p){
-
-	 var positions,
-	     paddles,
-	     moves,
-		sound;
-
-	 positions = Rx.Observable.fromEvent(document,'positions').
-		 map(function(o){return o.detail;});
-
-	 paddles = positions.
-		 filter(function(x){ return x.type === 'paddles'}).
-		 filter(function(x){
-				 return x.posy >= 80 && x.posy <= H-80; 
-				 });
-
-	 moves = positions.filter(function(b){ return b.type === 'moves';});
-	 moves.delay(16.66).map(move).do(draw).subscribe(dispatch);
-	 
-	 p.preload = function(){
-		 sound = p.loadSound('/assets/ping-pong-ball-hit.wav');	
-	 };
-	 
-	p.setup = function(){
+ function sketch(source,padds){
+	 return function(p){
 		 var ball,
 		     lPaddle,
 		     rPaddle,
-		     tmp;
-		 p.createCanvas(W,H);
-		 ball = {x: W/2, y: H/2, spdy: 1, spdx: 5};
-		 lPaddle = {x: 40, y: H/2};
-		 rPaddle = {x: W-40, y: H/2};
-		 tmp = {type: 'moves', ball: ball, lpad: lPaddle, rpad: rPaddle}
-		 p.rectMode(p.CENTER);
-		 dispatch(tmp);
-	 };
-	 function draw(objs){
-		var ball = objs.ball,
-		 rPaddle = objs.rpad,
-		 lPaddle = objs.lpad;
-		 p.background(3);
-		 p.rect(ball.x,ball.y,20,20);
-		 p.rect(rPaddle.x,rPaddle.y,20,80);
-		 p.rect(lPaddle.x,lPaddle.y,20,80);
-		 if(collisionX(objs) || collisionY(objs)){
-			sound.play();
-		}
-	 };
+		     sound;
+		 padds.do(function(o){
+			if(o.side === 'lpaddle'){
+				lPaddle.y = o.posy;
+			}else{
+				rPaddle.y = o.posy;
+			}
+		 }).subscribe(function(o){
+			console.log(o);
+		 });
+		 source.map(move).
+			 do(function(o){
+					 if(collisionX(o) || collisionY(o)){
+					 sound.play();
+					 }
+					 }).
+		 do(function(o){
+				 lPaddle.y = o.lpad.y;
+				 rPaddle.y = o.rpad.y;
+				 }).
+		 delay(16.66).subscribe(emitter);
+
+		 p.preload = function(){
+			 sound = p.loadSound('/assets/ping-pong-ball-hit.wav');	
+		 };
+
+		 p.setup = function(){
+			 var tmp;
+			 p.createCanvas(W,H);
+			 p.rectMode(p.CENTER);
+			 ball = {x: W/2, y: H/2, spdy: 1, spdx: 8};
+			 lPaddle = {x: 40, y: H/2};
+			 rPaddle = {x: W-40, y: H/2};
+			 tmp = {type: 'moves', ball: ball, lpad: lPaddle, rpad: rPaddle};
+			 emitter(tmp);
+		 };
+		 p.draw = function (){
+			 p.background(3);
+			 p.rect(ball.x,ball.y,20,20);
+			 p.rect(rPaddle.x,rPaddle.y,20,80);
+			 p.rect(lPaddle.x,lPaddle.y,20,80);
+		 };
+	 }
  }
 
  tcp = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
@@ -105,7 +107,7 @@ function collisionY(o){
 
  observer = Rx.Observer.create(
 		 function(n){
-		 dispatch(n);},
+		 emitter(n);},
 		 function(e){},
 		 function(){});
 
@@ -115,5 +117,18 @@ function collisionY(o){
 			 });
 
  messages.subscribe(observer);
- new p5(sketch,'board');
+
+ positions = Rx.Observable.fromEvent(document,'positions').
+	 map(function(o){return o.detail;});
+
+ paddles = positions.
+	 filter(function(x){ return x.type === 'paddles'}).
+	 filter(function(x){
+			 return x.posy >= 80 && x.posy <= H-80; 
+			 });
+
+
+ moves = positions.filter(function(b){ return b.type === 'moves';});
+
+ new p5(sketch(moves,paddles),'board');
 })();
