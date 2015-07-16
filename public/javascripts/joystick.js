665441ce963a3491d0d5b4c8e76c316d181e5263
javier salcedo <javiersalcedo75@gmail.com>
2015-07-15 22:15:17 -0300

    refactor of joystick.js a little more beauty

--- public/javascripts/joystick.js
+++ public/javascripts/joystick.js
@@ -1,61 +1,57 @@
 (function(){
 
-var ws, 
-eventstreams, 
-observer;
+ var ws, 
+ eventstreams, 
+ observer,
+ host,
+ path,
+ tcp;
 
-var host,
-path,
-tcp;
+ tcp = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+ host = window.location.host;
+ path = window.location.pathname;
+ ws = new WebSocket(tcp+host+'/ws'+path);
 
-tcp = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
-host = window.location.host;
-path = window.location.pathname;
-ws = new WebSocket(tcp+host+'/ws'+path);
 
-eventstreams = Rx.Observable.fromEvent(document,'paddlemove')
-.map(function(x){
-	return x.detail;
-});
-observer = Rx.Observer.create(
-	function(n){
-		ws.send(JSON.stringify(n));
-	},
-	function(e){ console.log(e);},
-	function(){});
+ function sketch(websocket){
+ return function(p){
+ var w = window.innerWidth,
+ h = window.innerHeight/2,
+ posY = h/2;
 
-eventstreams.subscribe(observer);
+ p.setup = function() {
+	 p.createCanvas(w, h);
+ };
 
-function sketch(p){
-var w = window.innerWidth,
-h = window.innerHeight/2,
-posY = h/2;
+ p.draw = function(){
+	 p.clear();
+	 p.rectMode(p.CENTER);
+	 p.fill(0);
+	 p.rect(w/2,h/2,(w/3)-200,h-10);
+	 p.ellipseMode(p.CENTER);
+	 p.fill(255,0,0);
+	 p.noStroke();
+	 p.ellipse(w/2,posY, w/3,w/3);
+ };
 
-p.setup = function() {
-	p.createCanvas(w, h);
-};
 
-p.draw = function(){
-	p.clear();
-	p.rectMode(p.CENTER);
-	p.fill(0);
-	p.rect(w/2,h/2,(w/3)-200,h-10);
-	p.ellipseMode(p.CENTER);
-	p.fill(255,0,0);
-	p.noStroke();
-	p.ellipse(w/2,posY, w/3,w/3);
-};
+ Rx.Observable.create(function(o){
+		 p.touchMoved = function(){
+		 posY = p.touchY;
+		 var y = (p.touchY * 450) / h;
+		 o.onNext({type: 'paddles', side: path.replace('/',''), posy: y});
+		 };
+		 }).
+		map(function(i){return JSON.stringify(i);}).
+		subscribe(function(m){
+			 websocket.send(m);
+			 });
 
-p.touchMoved = function(){
-	posY = p.touchY;
-	var y = (p.touchY * 450) / h;
-	document.dispatchEvent(new CustomEvent('paddlemove',
-	{'detail':{type: 'paddles', side: path.replace('/',''), posy: y}}));
-};
+ p.touchEnded = function(){
+	 posY = h/2;
+ };
 
-p.touchEnded = function(){
-	posY = h/2;
-};
-}
-new p5(sketch);
+ }
+ }
+ new p5(sketch(ws));
 })();
