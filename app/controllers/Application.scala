373f5b1e77e7a599ae546697d0a90e799eafdd26
javier salcedo <javiersalcedo75@gmail.com>
2015-07-21 22:10:46 -0300

    using FlowGraph and change another little things like css

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,60 +7,75 @@ import play.api.Play.current
 import scala.concurrent.Future
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Source, Flow, Sink}
+import akka.stream.scaladsl.{FlowGraph,Broadcast,Merge}
 import akka.stream.OverflowStrategy.dropHead
 import play.api.libs.json._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class Application extends Controller {
-  
-implicit val system = Akka.system
-import system.dispatcher
-implicit val materializer = ActorMaterializer()
-val streams = Source.actorRef[JsValue](0, dropHead)
 
-val sink = system.actorOf(Props[SinkActor], name = "sink")
+	val game = Akka.system.actorOf(Props[GameActor], name = "game")
 
-val srcRef = Flow[JsValue]
-	.to(Sink.actorRef(sink,"Completed"))
-	.runWith(streams)
-	
-  def index = Action { implicit request =>
-    Ok(views.html.index("Hello Pong"))
-  }
+		def index = Action { implicit request =>
+			Ok(views.html.index())
+		}
 
-  def socket = WebSocket.acceptWithActor[JsValue, JsValue] { request => wsRef =>
-      MyWebSocketActor.props(wsRef, srcRef, sink)
-  }
-	
-  def joystick = Action { implicit request =>
-  	Ok(views.html.joystick("Joystick"))
-  }
+	def socket = WebSocket.acceptWithActor[JsValue, JsValue] { request => wsRef =>
+		MyWebSocketActor.props(wsRef, game)
+	}
+
+	def joystick = Action { implicit request =>
+		Ok(views.html.joystick())
+	}
 
 }
 
 
 
 object MyWebSocketActor {
-  def props(wsRef: ActorRef, srcRef: ActorRef, sinkRef: ActorRef) = 
-	Props(new MyWebSocketActor(wsRef,srcRef,sinkRef))
-	case class Out(m:JsValue)
-	case object Subscribe
+	def props(wsRef: ActorRef, gameRef: ActorRef) = 
+		Props(new MyWebSocketActor(wsRef,gameRef))
+	case class Out(m:Any)
+			 case object Subscribe
 }
 import MyWebSocketActor._
-class MyWebSocketActor(wsRef: ActorRef, srcRef: ActorRef, sinkRef: ActorRef) extends 
-	Actor with ActorLogging {
-  override def preStart = sinkRef ! Subscribe
-  def receive = {
-    case Out(m) => wsRef ! m
-    case msg: JsValue =>
-      srcRef ! msg
-  }
+class MyWebSocketActor(wsRef: ActorRef, gameRef: ActorRef) extends 
+Actor with ActorLogging {
+	override def preStart = gameRef ! Subscribe
+		def receive = {
+			case Out(m) => wsRef ! m
+			case msg: JsValue =>
+				  gameRef ! msg
+		}
 }
 
-class SinkActor extends Actor with ActorLogging {
-	var outs = Set[ActorRef]()
-	def receive = {
-		case msg: JsValue =>  outs foreach{ _ ! Out(msg)}
-		case Subscribe => outs += sender()
-	}
+class GameActor extends Actor with ActorLogging {
+		implicit val system = context.system
+		import system.dispatcher
+		implicit val materializer = ActorMaterializer()
+
+		var outs = Set[ActorRef]()
+		val in = Source.actorRef[JsValue](0, dropHead)
+		val out = Sink.foreach[JsValue]{o => tellThem(o)}
+
+		val source = Flow() {implicit b =>
+			import FlowGraph.Implicits._
+				val bcast = b.add(Broadcast[JsValue](2))
+				val print = Sink.foreach[JsValue]{o => println(o.toString) }
+				val f1 = Flow[JsValue].filter(v => (v \ "type").asOpt[String].
+					getOrElse("").equals("paddles"))
+
+			bcast.out(0) ~> f1 ~> print
+
+			(bcast.in, bcast.out(1))
+		}
+
+	val sourceRef = (source.runWith(in,out))._1	
+		def receive = {
+			case msg: JsValue =>  sourceRef ! msg
+			case Subscribe => outs += sender()
+		}
+	def tellThem(msg:Any): Unit = outs foreach{ _ ! Out(msg)}
+
 }
 
--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -23,33 +23,57 @@
 		 var ball,
 		     lPaddle,
 		     rPaddle,
+		     lMarker,
+		     rMarker,
 		     sound;
 
 		 function isCollisionX(){
 			 var changed = (ball.x+10 > rPaddle.x-10 && 
-					 rPaddle.y+40 > ball.y-10 && rPaddle.y-40 < ball.y+10 ) ||
-				 (ball.x-10 < lPaddle.x+10 && (lPaddle.y+40 > ball.y-10 && 
-							       lPaddle.y-40 < ball.y+10 ));
+					 rPaddle.y+(rPaddle.h/2) > ball.y-10 && 
+					 rPaddle.y-(rPaddle.h/2) < ball.y+10 ) ||
+				 (ball.x-10 < lPaddle.x+10 && (lPaddle.y+(lPaddle.h/2) > ball.y-10 && 
+							       lPaddle.y-(lPaddle.h/2) < ball.y+10 ));
 			 return changed;
 		 }
 
+		 function incrementSpeed(){
+			 if(isCollisionX()){
+				 var i = 0.3;
+				 ball.spdx = ball.spdx > 0 ? ball.spdx + i : ball.spdx - i;
+			 }
+		 }
 
 		 function isCollisionY(){
 			 var changed = (ball.y > H-10 || ball.y < 10);
 			 return changed;
 		 }
 
-		 function moveAndSound(){
-			 if(isCollisionX() || isCollisionY()){
-				 sound.play();
+		 function playerWin(){
+			 if(lMarker.points == 15){
+				 p.draw = {}; 
+				 p.text('Left Won!',W/4,H/2);
+			 }else if(rMarker.points == 15){
+				 p.draw = {}; 
+				 p.text('Right Won!',W/4,H/2);
 			 }
-			 ball.spdx = isCollisionX() ? ball.spdx * -1 : ball.spdx;
-			 ball.spdy = isCollisionY() ? ball.spdy * -1 : ball.spdy;
+		 }
+
+		 function moveAndSound(){
 			 ball.x += ball.spdx;
 			 ball.y += ball.spdy;
-			 if(ball.x > W-40 || ball.x < 40){
+			 if(isCollisionX() || isCollisionY()){
+				 sound.play();
+				 ball.spdx = isCollisionX() ? ball.spdx * -1 : ball.spdx;
+				 ball.spdy = isCollisionY() ? ball.spdy * -1 : ball.spdy;
+				 incrementSpeed();
+			 }				
+			 if(ball.x > W || ball.x < 0){
+				 lMarker.points = ball.x > W-40 ? lMarker.points+1 : lMarker.points;
+				 rMarker.points = ball.x < 40 ? rMarker.points+1 : rMarker.points;
+				 playerWin();
 				 ball.x = W/2;
 				 ball.y = H/2;
+				 ball.spdx = ((ball.spdx-8 > 0 ? ball.spdx-8 : 2)/2)+8;
 			 }
 		 }
 
@@ -70,15 +94,23 @@
 			 p.createCanvas(W,H);
 			 p.rectMode(p.CENTER);
 			 ball = {x: W/2, y: H/2, spdy: 1, spdx: 8};
-			 lPaddle = {x: 40, y: H/2};
-			 rPaddle = {x: W-40, y: H/2};
+			 lPaddle = {x: 40, y: H/2, w: 20, h: 80};
+			 rPaddle = {x: W-40, y: H/2, w: 20, h: 80};
+			 p.textSize(100);
+			 p.textFont('Black Ops One');
+			 rMarker = {points:0};
+			 lMarker = {points:0};
 		 };
 		 p.draw = function (){
 			 p.background(3);
-			 p.rect(ball.x,ball.y,20,20);
-			 p.rect(rPaddle.x,rPaddle.y,20,80);
-			 p.rect(lPaddle.x,lPaddle.y,20,80);
 			 moveAndSound(); 
+			 p.fill(255,255,255,50);
+			 p.text(lMarker.points,100,H/3);
+			 p.text(rMarker.points,W-150,H/3);
+			 p.fill(255);
+			 p.rect(ball.x,ball.y,20,20);
+			 p.rect(rPaddle.x,rPaddle.y,rPaddle.w,rPaddle.h);
+			 p.rect(lPaddle.x,lPaddle.y,lPaddle.w,lPaddle.h);
 		 };
 	 }
  }
